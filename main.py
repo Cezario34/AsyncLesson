fc6856fddf01554c15437e4fc1c661a1861d92fc
Vladislav <gluhovvladislav514@gmail.com>
2026-03-05 06:40:06 +0300

    add async func

--- main.py
+++ main.py
@@ -7,9 +7,14 @@ from utils import async_timed, fetch_status
 @async_timed()
 async def main():
 	async with aiohttp.ClientSession() as session:
-		url = 'https://xn--b1aew.xn--p1ai/'
-		status = await fetch_status(session, url)
-		print(f'Состояние для {url} было равно {status}')
-
+		fetchers = \
+		[asyncio.create_task(fetch_status(session, 'https://example.com')),
+		asyncio.create_task(fetch_status(session, 'https://example.com'))]
+		done, pending = await asyncio.wait(fetchers)
+		print(f'Число завершившихся задач: {len(done)}')
+		print(f'Число ожидающих задач: {len(pending)}')
+		for done_task in done:
+			result = await done_task
+			print(result)
 
 asyncio.run(main())
